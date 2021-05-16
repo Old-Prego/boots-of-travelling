9a1312854f7b14fc5d4bda06c5782670dbc309f3
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-05-16 07:21:22 -0500

    Added item model

---
+++ models/Item.js
@@ -0,0 +1,40 @@
+const { Model, Datatypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/config');
+
+class Item extends Model{
+
+}
+
+Item.innit(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    defenseBoost:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    strengthBoost:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    DexterityBoost:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
+  }
+);
+module.exports = Item;
