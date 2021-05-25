a20ed82af7590de2cf7f861b454ecd9f8a5af5e4
Old Prego <spymasterluke@gmail.com>
2021-05-24 19:36:22 -0500

    Got skeleton of populating the stats sidebar set up

--- public/js/charSelect.js
+++ public/js/charSelect.js
@@ -1,5 +1,21 @@
+// const { player_character } = require('../../models');
+
 const startGame = async function() {
 
     document.location.replace('/gameplay/2');
       
 };
+
+// const popSidebar = async function(id) {
+    
+//     const characterData = await player_character.findByPk(id);
+
+//     const character = characterData.get({ plain: true });
+
+//     // set hp.innerHTML = character.hp
+//     strength = character.strength;
+
+// };
+
+// find hp
+// assign hp to variable
